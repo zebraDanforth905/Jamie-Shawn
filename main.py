a5da11f6d6a92ddf919ec237472a7783bbffbd49
GreatCoderJamie <johndoe@example.com>
2026-04-03 14:41:21 -0400

    Main file added

---
+++ main.py
@@ -0,0 +1,40 @@
+import pygame
+pygame.init()
+
+Clock = pygame.time.Clock()
+FPS = 60
+
+WIDTH = 1280
+HEIGHT = 720
+screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
+pygame.display.set_caption("Ride Rush")
+
+WHITE = [255,255,255]
+RED = [255,0,0]
+GREEN = [0,255,0]
+BLUE = [0,0,255]
+BLACK = [0,0,0]
+YELLOW = [255,255,0]
+PURPLE = [255,0,255]
+ORANGE = [255,180,0]
+#BACKGROUNDCOLOUR = []
+
+MenuScreen = []
+
+SettingsScreen = []
+
+SimulationScreen = []
+
+CurrentScreen = []
+
+isRunning = True
+while isRunning:
+    screen.fill(BLUE)
+    for ev in pygame.event.get():
+        if ev.type == pygame.QUIT:
+            isRunning = False
+    
+    pygame.display.update()
+    Clock.tick(FPS)
+
+pygame.quit()
