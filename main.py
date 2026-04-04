6e25ea390f739fcd25ea42870f41cdc460b77ae0
GreatCoderJamie <johndoe@example.com>
2026-04-03 21:50:53 -0400

    Added background Colour

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ FPS = 60
 
 WIDTH = 1280
 HEIGHT = 720
-screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Ride Rush")
 
 WHITE = [255,255,255]
@@ -17,7 +17,7 @@ BLACK = [0,0,0]
 YELLOW = [255,255,0]
 PURPLE = [255,0,255]
 ORANGE = [255,180,0]
-#BACKGROUNDCOLOUR = []
+BACKGROUNDCOLOUR = [115, 147, 179]
 
 MenuScreen = []
 
@@ -29,11 +29,12 @@ CurrentScreen = []
 
 isRunning = True
 while isRunning:
-    screen.fill(BLUE)
+    screen.fill(BACKGROUNDCOLOUR)
     for ev in pygame.event.get():
         if ev.type == pygame.QUIT:
             isRunning = False
-    
+        elif ev.type == pygame.MOUSEBUTTONDOWN:
+            print(pygame.mouse.get_pos())
     pygame.display.update()
     Clock.tick(FPS)
 
