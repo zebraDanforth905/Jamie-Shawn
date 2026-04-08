b7da211176804ff31066463d4ea1bbb25a38b4a2
GreatCoderJamie <johndoe@example.com>
2026-04-08 16:56:41 -0400

    Added the file for the unique theme park things

---
+++ ThemePark.py
@@ -0,0 +1,6 @@
+#This file is for the rendering of the unique rides and fuel stations
+#Import libraries
+import pygame
+pygame.init()
+from ScreenElements import Rectangle
+from ScreenElements import Text
