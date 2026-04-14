cf7187add49e696b9ffdc66693abceed7bb54100
GreatCoderJamie <johndoe@example.com>
2026-04-14 17:28:04 -0400

    cool

--- ThemePark.py
+++ ThemePark.py
@@ -6,7 +6,7 @@ from ScreenElements import Rectangle
 from ScreenElements import Text
 from RideData import Ride_data
 class Attraction:
-    def __init__(self, screen, name, backgroundcolor, width, height, x, y):
+    def __init__(self, screen, name, backgroundcolor, width, height, x, y, type):
         self.screen = screen
         self.name = name
         self.backgroundcolor = backgroundcolor
@@ -14,11 +14,14 @@ class Attraction:
         self.height = height
         self.x = x
         self.y = y
+        self.type = type
         self.visible = False
         self.font = pygame.font.Font(None, 30)
         self.rect = Rectangle(self.screen, None, None, None, self.x, self.y, self.width, self.height, 0, None, self.backgroundcolor, None, None)
         self.waitTime = 0
         self.satisfaction = 75
+        self.itemsSold = 20
+        self.sales = 100
         self.fixed = False
     def alert(self):
         pass
@@ -28,5 +31,9 @@ class Attraction:
         self.rect.update()
         self.render = self.font.render(self.name, True, [0, 0, 0], None)
         self.screen.blit(self.render, self.render.get_rect(center=(self.x + self.width/2, self.y + self.height/2)))
-        if self.waitTime > 30 or self.satisfaction < 75:
-            self.alert()
+        if self.type == "Ride" and not self.fixed:
+            if self.waitTime > 30 or self.satisfaction < 75:
+                self.alert()
+        elif self.type == "Concenssion" and not self.fixed:
+            if self.itemsSold < 20:
+                self.alert()
