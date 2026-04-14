b91b2473345b98f18ff73f127ac3f0a9f25a1191
GreatCoderJamie <johndoe@example.com>
2026-04-14 17:19:18 -0400

    hi

--- ThemePark.py
+++ ThemePark.py
@@ -17,7 +17,16 @@ class Attraction:
         self.visible = False
         self.font = pygame.font.Font(None, 30)
         self.rect = Rectangle(self.screen, None, None, None, self.x, self.y, self.width, self.height, 0, None, self.backgroundcolor, None, None)
+        self.waitTime = 0
+        self.satisfaction = 75
+        self.fixed = False
+    def alert(self):
+        pass
     def update(self, time):
+        self.waitTime = Ride_data[time][self.name]["wait"]
+        self.satisfaction = Ride_data[time][self.name]["satisfaction"]
         self.rect.update()
         self.render = self.font.render(self.name, True, [0, 0, 0], None)
         self.screen.blit(self.render, self.render.get_rect(center=(self.x + self.width/2, self.y + self.height/2)))
+        if self.waitTime > 30 or self.satisfaction < 75:
+            self.alert()
