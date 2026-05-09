91f9e42dee15fca3f4b9af6e223d7d23d25219f7
GreatCoderJamie <johndoe@example.com>
2026-05-09 17:23:06 -0400

    fixed a bug that caused it to crash

--- ScreenElements.py
+++ ScreenElements.py
@@ -5,7 +5,7 @@ pygame.font.init()
 
 #Generic Object Class
 class Object:
-    def __init__(self, screen, clickable=None, clickingType=None, clickScreen=None, clickValue=None, x=0, y=0, width=1, height=1, borderWidth=0, colour=None, backgroundColour=None, Font=None, Text=None, visible=True):
+    def __init__(self, screen, clickable=None, clickingType=None, clickScreen=None, x=0, y=0, width=1, height=1, borderWidth=0, colour=None, backgroundColour=None, Font=None, Text=None, visible=True, clickValue=None):
         #Screen/Drawing-location
         self.screen = screen
         #Object position
