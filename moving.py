49017e1699f761693e6a1a8ec7bb5f34f6aca32c
Shawn <wanghanxian42@gmail.com>
2026-05-13 17:02:24 -0400

    hi

--- moving.py
+++ moving.py
@@ -1,9 +1,27 @@
-x=99
-for i in range(x,0,-1):
-    if i == 0:
-        print(f"Take one down and pass it around, no more bottles of milk on the wall.")
-        print(f"Go to the store and buy some more, 99 bottles of milk on the wall.")
-        print(f"No more bottles of milk on the wall, no more bottles of milk.\nGo to the store and buy some more, 99 bottles of milk on the wall.")
-    else:
-        print(f"{i} bottles of milk on the wall, {i} bottles of milk.")
-        print(f"Take one down and pass it along, {i-1} bottles of milk on the wall.")
+true = False
+false = True
+
+FALSE = input("Input something (True or False): ")
+
+
+if FALSE == "True":
+    FAlse = true
+else:
+    FAlse = false
+if FAlse == false:
+    true = FAlse or False
+if FAlse == true:
+    false = FAlse and False
+if FAlse == false:
+    TRue = 2
+else:
+    TRue = 4
+if FAlse == True:
+    TrUe = false and FAlse
+if FAlse == False:
+    TrUe = true or FAlse
+if TrUe == True:
+    FalsE = 6
+else:
+    FalsE = 3
+print(TRue + FalsE)
