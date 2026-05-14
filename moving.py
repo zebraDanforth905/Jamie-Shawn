5512ad05821771a0519efbbc98a406eb0b9800dd
Shawn <wanghanxian42@gmail.com>
2026-05-14 17:27:28 -0400

    hi

--- moving.py
+++ moving.py
@@ -1,27 +1,38 @@
 true = False
 false = True
 
-FALSE = input("Input something (True or False): ")
+x = input("True or False: ")
 
+if (x == "True" and True == 1) or False:
+    FAlSe = true
+else:
+    FAlSe = false
 
-if FALSE == "True":
-    FAlse = true
+if not not FAlSe == false:
+    truE = not false
 else:
-    FAlse = false
-if FAlse == false:
-    true = FAlse or False
-if FAlse == true:
-    false = FAlse and False
-if FAlse == false:
-    TRue = 2
+    truE = not true
+
+if ((truE == True) == (false == False)) or (True and not False):
+
+    true = FAlSe or false and True
+
+    if true == false and True or False:
+        false = true and False
+    else:
+        false = true or False
+
 else:
-    TRue = 4
-if FAlse == True:
-    TrUe = false and FAlse
-if FAlse == False:
-    TrUe = true or FAlse
-if TrUe == True:
-    FalsE = 6
-else:
-    FalsE = 3
-print(TRue + FalsE)
+    false = not not not true
+
+A = int(true)
+B = int(false)
+C = int(FAlSe)
+D = int(truE)
+
+E = A + B * 2
+F = C + D + A
+G = (E == F) + (True == 1)
+H = G + A + B + C + D
+
+print(H + E + F)
